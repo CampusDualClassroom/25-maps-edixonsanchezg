c528c177c6fbb7e8861bbafd40ad5e419a85547d
Edixon Sánchez <ediiixonsanchezg@gmail.com>
2025-11-04 20:25:35 +0100

    ejercicio 25 completado

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,19 +1,43 @@
 package com.campusdual.classroom;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Exercise {
 
     public static Map<String, Person> createHashMap() {
+        Map<String, Person> map = new HashMap<>();
 
+        map.put("person", new Person("John","Smith"));
+        map.put("teacher", new Teacher("María","Montessori","Educación"));
+        map.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        map.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return map;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key, value); // Si la clave existe, sobrescribe el valor
+        return value;
     }
 
     public static void printMapValues(Map<String, Person> map) {
+        for (Map.Entry<String, Person> entry : map.entrySet()) {
+            entry.getValue().getDetails(); // Polimorfismo: llama al getDetails() correcto según la subclase
+        }
 
+
+
+
+    }
+    public static void main(String[] args) {
+        Map<String, Person> myMap = createHashMap();
+
+        System.out.println("Detalles iniciales del mapa:");
+        printMapValues(myMap);
+
+        System.out.println("\nAñadiendo nuevo policía:");
+        addMapValue(myMap, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));
+        printMapValues(myMap);
     }
 
 }
